da3209a810a30106ae1c796dd08109efb49e0267
WhiteSeven <jjiuwennitoufayouduochang@gmail.com>
2024-03-03 16:25:08 +0800

    game一点点

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ GAMESHIFT = UserEvent()
 BLOCK_DOWN =  UserEvent()
 NEED_NEXT =  UserEvent()
 ELIMINATE_ROWS =  UserEvent()
+# 
+CAN_CONTROL = UserEvent()
 
 
 # 按钮
@@ -125,10 +127,79 @@ class Block:
 
 
 class BlockSys:
+    ROW = 23
+    COLUMN = 10
+
     def __init__(self) -> None:
         self.map: dict[tuple[int, int], Block] = {}
+        self.clicked: dict[Block, pygame.Vector2] = []
     
+
     def update(self, qst: Qst):
+        self.contorl()
+        self.down()
+
+
+    def contorl(self):
+        for event in pygame.event.get(pygame.KEYDOWN):
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                self.move(pygame.Vector2(-1, 0))
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                self.move(pygame.Vector2(1, 0))
+            elif event.key in (pygame.K_SPACE, pygame.K_w, pygame.K_UP):
+                ...
+
+    
+    def down(self):
+        if not pygame.event.get(BLOCK_DOWN):
+            return
+        # 检测能不能下落
+        for vect in self.clicked.values():
+            if tuple(vect + pygame.Vector2(0, 1)) in self.map or vect.y >= self.ROW:
+                self.put()
+                return
+        # 下落
+        for block in self.clicked:
+            self.clicked[block].y += 1
+        pygame.time.set_timer(BLOCK_DOWN, 1000, 1)
+
+
+    def put(self):
+        """把手上的砖放在map上"""
+        for block, vect in self.clicked.items():
+            self.map[tuple(vect)] = block
+        self.clicked.clear()
+        if dead_row := self.ckeck_full():
+            self.kill_row(dead_row)
+
+    
+    def draw(self):
+        ...
+
+
+    def ckeck_full(self) -> list[int]:
+        dead_row = []
+        for row in range(self.ROW - 1, -1, -1):
+            for column in random(self.COLUMN):
+                if (column, row) not in self.map:
+                    break
+            else:
+                dead_row.append(row)
+        return dead_row
+    
+
+    def move(self, vect: pygame.Vector2):
+        # 检测能不能移动
+        for vector in self.clicked.values():
+            new_vect = vector + vect
+            if tuple(new_vect) in self.map or new_vect.x < 0 or new_vect.x >= self.COLUMN:
+                return
+        # 移动
+        for vector in self.clicked.values():
+            vector += vect
+
+
+    def kill_row(self, dead_row: list[int]):
         ...
 
 
@@ -154,7 +225,7 @@ class Windows:
                 pygame.quit()
                 sys.exit()
             self.update()
-            pygame.event.clear()
+            # pygame.event.clear()
             pygame.display.flip()
             self.clock.tick(60)
 
@@ -189,7 +260,6 @@ class Game(Windows):
 
         self.set_mode()
         return super().update()
-
 
 
 Game().run()
